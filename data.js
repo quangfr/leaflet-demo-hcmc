aefd0d16a238bf675fa3c354d0588c1a29ee78da
Quang Tran <quangfr@users.noreply.github.com>
2017-06-18 01:04:42 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,7 @@
+
+//An extract of address points from the LINZ bulk extract: http://www.linz.govt.nz/survey-titles/landonline-data/landonline-bde
+//Should be this data set: http://data.linz.govt.nz/#/layer/779-nz-street-address-electoral/
+var addressPoints = [
+    [10.7567, 106.6649, "Bênh viện Phạm Ngọc Thạch","120 Hồng Bàng, phường 12, Quận 5","+84 8 3855 0207","Bác sỹ A", '5900113accd14d004413fc54','18/05 13:00','18/05 15:30', '19/05 18:30'],
+    [10.75579, 106.66168, "Bênh viện Chợ Rẫy","201B Nguyễn Chí Thanh, phường 12, Quận 5","+84 8 3955 9856", "Bác sỹ B",'5900113accd14d004413fc55','17/05 08:00','18/05 10:00', '18/05 14:00']
+];
